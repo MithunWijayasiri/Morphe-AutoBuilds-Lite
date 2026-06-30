721a115306c72bcbaf690bec111e429b60703511
Mithun Wijayasiri <mithun.wys@gmail.com>
2026-07-01 00:08:38 +0530

    chore: remove dead code (r2 uploader, release.py, unused scripts/functions)

--- src/__init__.py
+++ src/__init__.py
@@ -16,10 +16,6 @@ logging.basicConfig(
 # Env Vars
 github_token = os.getenv('GITHUB_TOKEN') or os.getenv('GH_TOKEN')
 repository = os.getenv('GITHUB_REPOSITORY')
-endpoint_url = os.getenv('ENDPOINT_URL')
-access_key_id = os.getenv('AWS_ACCESS_KEY_ID')
-secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY')
-bucket_name = os.getenv('BUCKET_NAME')
 
 # APKmirror base url
 base_url = "https://www.apkmirror.com"
